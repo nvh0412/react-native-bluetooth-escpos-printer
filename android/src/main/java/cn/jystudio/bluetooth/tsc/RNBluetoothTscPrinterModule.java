dac3ee1160b0699ee49cbdb5d7abfeffb3860d71
Hoa Nguyen <hoa.nguyen@employmenthero.com>
2020-02-01 21:25:49 +0700

    Fix android printer

--- android/src/main/java/cn/jystudio/bluetooth/tsc/RNBluetoothTscPrinterModule.java
+++ android/src/main/java/cn/jystudio/bluetooth/tsc/RNBluetoothTscPrinterModule.java
@@ -6,6 +6,7 @@ import android.util.Base64;
 import cn.jystudio.bluetooth.BluetoothService;
 import cn.jystudio.bluetooth.BluetoothServiceStateObserver;
 import com.facebook.react.bridge.*;
+import com.nyear.lib.sdk.bean.NyearElementBean;
 
 import java.util.Map;
 import java.util.Vector;
@@ -33,157 +34,44 @@ implements BluetoothServiceStateObserver{
     public void printLabel(final ReadableMap options, final Promise promise) {
         int width = options.getInt("width");
         int height = options.getInt("height");
-        int gap = options.hasKey("gap") ? options.getInt("gap") : 0;
-        TscCommand.SPEED speed = options.hasKey("speed")?this.findSpeed(options.getInt("speed")):null;
-        TscCommand.ENABLE enable = options.hasKey("tear") ?
-                options.getString("tear").equalsIgnoreCase(TscCommand.ENABLE.ON.getValue()) ? TscCommand.ENABLE.ON : TscCommand.ENABLE.OFF
-                : TscCommand.ENABLE.OFF;
+
         ReadableArray texts = options.hasKey("text")? options.getArray("text"):null;
-        ReadableArray qrCodes = options.hasKey("qrcode")? options.getArray("qrcode"):null;
-        ReadableArray barCodes = options.hasKey("barcode")? options.getArray("barcode"):null;
-        ReadableArray images = options.hasKey("image")? options.getArray("image"):null;
-        ReadableArray reverses = options.hasKey("reverse")? options.getArray("reverse"):null;
 
-        TscCommand.DIRECTION direction = options.hasKey("direction") ?
-                TscCommand.DIRECTION.BACKWARD.getValue() == options.getInt("direction") ? TscCommand.DIRECTION.BACKWARD : TscCommand.DIRECTION.FORWARD
-                : TscCommand.DIRECTION.FORWARD;
-//        Not Support Yet
-//        TscCommand.MIRROR mirror = options.hasKey("mirror") ?
-//                TscCommand.MIRROR.MIRROR.getValue() == options.getInt("mirror") ? TscCommand.MIRROR.MIRROR : TscCommand.MIRROR.NORMAL
-//                : TscCommand.MIRROR.NORMAL;
-        TscCommand.DENSITY density = options.hasKey("density")?this.findDensity(options.getInt("density")):null;
-        ReadableArray reference = options.hasKey("reference")?options.getArray("reference"):null;
-
-        boolean sound = false;
-        if (options.hasKey("sound") && options.getInt("sound") == 1) {
-            sound = true;
-        }
-        boolean home = false;
-        if(options.hasKey("home") && options.getInt("home")== 1){
-            home = true;
-        }
         TscCommand tsc = new TscCommand();
-        if(speed != null){
-            tsc.addSpeed(speed);//设置打印速度
-        }
-        if(density != null){
-            tsc.addDensity(density);//设置打印浓度
-        }
-        tsc.addSize(width,height); //设置标签尺寸，按照实际尺寸设置
-        tsc.addGap(gap);           //设置标签间隙，按照实际尺寸设置，如果为无间隙纸则设置为0
-         tsc.addDirection(direction);//设置打印方向
-        //设置原点坐标
-        if (reference != null && reference.size() == 2) {
-            tsc.addReference(reference.getInt(0), reference.getInt(1));
-        } else {
-            tsc.addReference(0, 0);
-        }
-        tsc.addTear(enable); //撕纸模式开启
-        if(home) {
-            tsc.addBackFeed(16);
-            tsc.addHome();//走纸到开始位置
-        }
-        tsc.addCls();// 清除打印缓冲区
-        //绘制简体中文
+
+        tsc.addSize(width,height);
+        tsc.addCls();
+
         for (int i = 0;texts!=null&& i < texts.size(); i++) {
+            final NyearElementBean bean = new NyearElementBean();
             ReadableMap text = texts.getMap(i);
             String t = text.getString("text");
+
             int x = text.getInt("x");
             int y = text.getInt("y");
-            TscCommand.FONTTYPE fonttype = this.findFontType(text.getString("fonttype"));
-            TscCommand.ROTATION rotation = this.findRotation(text.getInt("rotation"));
-            TscCommand.FONTMUL xscal = this.findFontMul(text.getInt("xscal"));
-            TscCommand.FONTMUL yscal = this.findFontMul(text.getInt("xscal"));
+
+            bean.setXBit(x);
+            bean.setYBit(y);
+
             boolean bold = text.hasKey("bold") && text.getBoolean("bold");
 
             try {
                 byte[] temp = t.getBytes("UTF-8");
                 String temStr = new String(temp, "UTF-8");
-                t = new String(temStr.getBytes("GB2312"), "GB2312");//打印的文字
+                t = new String(temStr.getBytes("UTF-8"), "UTF-8");
+
+                bean.setContent(t);
             } catch (Exception e) {
                 promise.reject("INVALID_TEXT", e);
                 return;
             }
 
-            tsc.addText(x, y, fonttype/*字体类型*/,
-                    rotation/*旋转角度*/, xscal/*横向放大*/, yscal/*纵向放大*/, t);
-
             if(bold){
-                tsc.addText(x+1, y, fonttype,
-                        rotation, xscal, yscal, t/*这里的t可能需要替换成同等长度的空格*/);
-                tsc.addText(x, y+1, fonttype,
-                        rotation, xscal, yscal, t/*这里的t可能需要替换成同等长度的空格*/);
+                bean.setBold(true);
             }
         }
 
-        //绘制图片
-        if(images != null){
-            for (int i = 0; i < images.size(); i++) {
-                ReadableMap img = images.getMap(i);
-                int x = img.getInt("x");
-                int y = img.getInt("y");
-                int imgWidth = img.getInt("width");
-                TscCommand.BITMAP_MODE mode = this.findBitmapMode(img.getInt("mode"));
-                String image  = img.getString("image");
-                byte[] decoded = Base64.decode(image, Base64.DEFAULT);
-                Bitmap b = BitmapFactory.decodeByteArray(decoded, 0, decoded.length);
-                tsc.addBitmap(x,y, mode, imgWidth,b);
-            }
-        }
-
-        if (qrCodes != null) {
-            for (int i = 0; i < qrCodes.size(); i++) {
-                ReadableMap qr = qrCodes.getMap(i);
-                int x = qr.getInt("x");
-                int y = qr.getInt("y");
-                int qrWidth = qr.getInt("width");
-                TscCommand.EEC level = this.findEEC(qr.getString("level"));
-                TscCommand.ROTATION rotation = this.findRotation(qr.getInt("rotation"));
-                String code = qr.getString("code");
-                tsc.addQRCode(x, y, level, qrWidth, rotation, code);
-            }
-        }
-        if (barCodes != null) {
-            for (int i = 0; i < barCodes.size(); i++) {
-                ReadableMap bar = barCodes.getMap(i);
-                int x = bar.hasKey("x") ? bar.getInt("x") : 0;
-                int y = bar.hasKey("y") ? bar.getInt("y") : 0;
-                int barHeight = bar.hasKey("height") ? bar.getInt("height") : 200;
-                int barWide = bar.hasKey("wide") ? bar.getInt("wide") : 2;
-                int narrow = bar.hasKey("narrow") ? bar.getInt("narrow") : 1;
-                TscCommand.ROTATION rotation = this.findRotation(bar.getInt("rotation"));
-                String code = bar.getString("code");
-                TscCommand.BARCODETYPE type = this.findBarcodeType(bar.getString("type"));
-                TscCommand.READABLE readable = this.findReadable(bar.getInt("readable"));
-                tsc.add1DBarcode(x, y, type, barHeight, barWide, narrow, readable, rotation, code);
-            }
-        }
-
-        if(reverses != null){
-            for(int i=0; i < reverses.size(); i++){
-                ReadableMap area = reverses.getMap(i);
-                int ax = area.getInt("x");
-                int ay = area.getInt("y");
-                int aWidth = area.getInt("width");
-                int aHeight = area.getInt("height");
-                tsc.addReverse(ax,ay,aWidth,aHeight);
-            }
-        }
-
-        tsc.addPrint(1, 1); // 打印标签
-        if (sound) {
-            tsc.addSound(2, 100); //打印标签后 蜂鸣器响
-        }
-        Vector<Byte> bytes = tsc.getCommand();
-        byte[] tosend = new byte[bytes.size()];
-        for(int i=0;i<bytes.size();i++){
-            tosend[i]= bytes.get(i);
-        }
-        if(sendDataByte(tosend)){
-            promise.resolve(null);
-        }else{
-            promise.reject("COMMAND_SEND_ERROR");
-        }
+        promise.resolve(null);
     }
 
     private TscCommand.BARCODETYPE findBarcodeType(String type) {
